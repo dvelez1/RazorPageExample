bbf4cddab9e9ae5edc0a5bf28b4cb8ea0661ce56
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-04-30 17:48:45 -0400

    localDbChanges

---
+++ Create dbo.Positions.sql
@@ -0,0 +1,9 @@
+﻿USE [Employees]
+GO
+
+select * from [dbo].[Positions] 
+
+update [dbo].[Positions] 
+set PositionDescription = 'Human Resrouces Supervisor'
+where PositionId =1
+select * from [dbo].[Positions] 
